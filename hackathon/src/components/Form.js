5e0e3b6addbbf1cd8a4294a93c626d635c384515
Jason Soo <jasn9776@gmail.com>
2021-09-11 17:29:28 +1000

    added new changes to form

--- hackathon/src/components/Form.js
+++ hackathon/src/components/Form.js
@@ -2,26 +2,44 @@ import {useState} from 'react'
 
 const Form = () => {
 
-    const [text, setField] = useState('')
-    const [day, setName] = useState('')
+    const [text, setText] = useState('')
+    const [name, setName] = useState('')
     const [symptoms, setSymptoms] = useState(false)
 
 
     return (
-        <div>
-            <label>Task</label>
-            <input
-            type='text'
-            placeholder='Field'
-            value = {text}
-            />
-            <input
-            type='checkbox'
-            checked={symptoms}
-            value={symptoms}
-            onChange={(e) => setSymptoms(e.currentTarget.checked)}
-            />
-        </div>
+        <form className='add-form' 
+        // onSubmit={onSubmit}
+        >
+            <div className='form-control'>
+                <label>Patient Name</label>
+                <input
+                    type='text'
+                    placeholder='Name'
+                    value = {name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className="form-control">
+                <label>Description of Symptoms</label>
+                <input
+                    type='text'
+                    placeholder='Description of Symptoms'
+                    value = {text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+            </div>
+            <div className="form-control form-control-check">
+                <label>Symptoms?</label>
+                <input
+                type='checkbox'
+                checked={symptoms}
+                value={symptoms}
+                onChange={(e) => setSymptoms(e.currentTarget.value)}
+                />
+                <input type='submit' value='Save Task' className='btn btn-block' />
+            </div>
+        </form>
     )
 
 }
